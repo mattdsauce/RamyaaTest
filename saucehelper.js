b72895dfa832fa41a9db1fe37d3405e3166f0262
Matt Dunn <mattd@saucelabs.com>
2016-07-11 13:51:42 +0100

    first upload

---
+++ saucehelper.js
@@ -0,0 +1,39 @@
+var shell = require('shelljs');
+var webdriver = require('selenium-webdriver');
+var test = require('selenium-webdriver/testing');
+
+
+test.describe('SauceLabs Helper', function() {
+
+  test.it('Go to Google', function() {
+    if(process.env.SAUCELABS) {
+        var username = process.env.SAUCE_USERNAME;
+        var accessKey = process.env.SAUCE_ACCESS_KEY;
+        driver = new webdriver.Builder().
+        withCapabilities({
+          'browserName': process.env.SELENIUM_BROWSER,
+          'platform': process.env.SELENIUM_PLATFORM,
+          'version': 'latest',
+          'username': username,
+          'accessKey': accessKey,
+        }).
+        usingServer("http://" + username + ":" + accessKey +
+        "@ondemand.saucelabs.com:80/wd/hub").
+        build();
+        var remote = require('selenium-webdriver/remote');
+        driver.setFileDetector(new remote.FileDetector());
+      } else {
+        driver = new webdriver.Builder().
+        withCapabilities(webdriver.Capabilities.chrome()).
+        build();
+      }
+      driver.manage().window().maximize();
+      driver.get("http://google.com");
+      console.log("Sleeping for 10 seconds");
+      driver.sleep(10000);
+  });
+
+  test.afterEach(function() {
+    driver.quit();
+  });
+});
